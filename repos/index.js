c4985ac0e4de17ff7a57a64304e356e91623e05b
franciscog <frankjg@gmail.com>
2020-07-26 14:05:56 -0500

    exporting all sub-repos as one object

---
+++ repos/index.js
@@ -0,0 +1,5 @@
+exports.triggers = require('./triggers');
+exports.users = require('./users');
+exports.leaderboard = require('./leaderboard');
+exports.songStats = require('./song_stats');
+exports.songIssues = require('./song_issues');
